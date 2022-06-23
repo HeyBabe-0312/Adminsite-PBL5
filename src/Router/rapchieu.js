4130d0a330806aeca4e83615161b7648802e5a0f
ホー・ミン・ヒエウ <oppahd96@gmail.com>
2022-06-23 12:32:53 +0700

    Update rapchieu.js

--- src/Router/rapchieu.js
+++ src/Router/rapchieu.js
@@ -23,26 +23,13 @@ const Rapchieu = ({modalOpenDetail,roomId,roomName,roomStatus,roomIdLC}) => {
   )
 }
 function xuliDate(val){
-  var date = new Date(val);
-  var year = date.getFullYear();
-  var month = date.getMonth()+1;
-  var dt = date.getDate();
-  var hour = date.getHours();
-  var minute = date.getMinutes();
-  if (hour < 10) {
-    hour = '0' + hour;
-  }
-  if (minute < 10) {
-    minute = '0' + minute;
-  }
-  if (dt < 10) {
-    dt = '0' + dt;
-  }
-  if (month < 10) {
-    month = '0' + month;
-  }
+  var year = val.substring(0,4);
+  var month = val.substring(5,7);
+  var dt = val.substring(8,10);
+  var hour = val.substring(11,13);
+  var minute = val.substring(14,16);
+
   var day = hour + 'h' + minute + " " + dt +'/' + month + '/'+ year;
   return(day);
 }
-
 export default Rapchieu;
